8122dc56600dbea7916201517cd8a2bfe554835c
Arvind Kulkarni <aravinda.kulkarni@gmail.com>
2025-06-05 15:18:12 +0530

    commit

---
+++ src/ex_01_Java_Basic/Lab002_extra.java
@@ -0,0 +1,7 @@
+package ex_01_Java_Basic;
+
+public class Lab002_extra {
+    public static void main(String[] args) {
+        System.out.println("I am Arvind Kulkarni,learning Java programming");
+    }
+}
