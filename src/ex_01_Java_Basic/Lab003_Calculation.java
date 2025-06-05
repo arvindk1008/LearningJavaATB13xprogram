f00573a27c6150846eace9b720426a50d247dff2
Arvind Kulkarni <aravinda.kulkarni@gmail.com>
2025-06-05 15:38:51 +0530

    add03

--- src/ex_01_Java_Basic/Lab003_Calculation.java
+++ src/ex_01_Java_Basic/Lab003_Calculation.java
@@ -2,6 +2,6 @@ package ex_01_Java_Basic;
 
 public class Lab003_Calculation {
     public static void main(String[] args) {
-        System.out.println("2+2");
+        System.out.println("2+2=4");
     }
 }
