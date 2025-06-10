277d4e4243203cf66df1147a3273c9e068d9b1b3
Arvind Kulkarni <aravinda.kulkarni@gmail.com>
2025-06-10 16:20:53 +0530

    addLab001

---
+++ src/ex_02_Java_sample01/multiplication_table.java
@@ -0,0 +1,10 @@
+package ex_02_Java_sample01;
+
+public class multiplication_table {
+    public static void main(String[] args) {
+        int number=9;
+        for (int i=1;i<=10;i++) {
+            System.out.println(number + "*" + i + "="+(number*i));
+        }
+    }
+}
