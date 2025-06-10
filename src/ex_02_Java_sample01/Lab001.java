9bb74819b53f5ffac8ca8cccc36bd6a52ea5f392
Arvind Kulkarni <aravinda.kulkarni@gmail.com>
2025-06-10 15:23:49 +0530

    addLab001

--- src/ex_02_Java_sample01/Lab001.java
+++ src/ex_02_Java_sample01/Lab001.java
@@ -1,7 +1,12 @@
 package ex_02_Java_sample01;
 
 public class Lab001 {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+        int p=25,q=20;
+        System.out.println("First Number is "+p);
+        System.out.println("Second Number is" +q);
+        System.out.println("Sum of two numbers is "+(p+q));
         System.out.println("This is my first program");
     }
 }
