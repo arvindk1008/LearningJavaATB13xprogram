23e79f4de5b49c454a329dcb43f8d8aec03aa3dd
Arvind Kulkarni <aravinda.kulkarni@gmail.com>
2025-06-10 16:57:53 +0530

    addLab001

---
+++ src/ex_01_Java_Basic/Comments_Multi.java
@@ -0,0 +1,12 @@
+package ex_01_Java_Basic;
+
+public class Comments_Multi {
+    public static void main(String[] args) {
+        /*
+        * Author: Arvind Kulkarni
+        * Status: Looking for a Job
+        */
+      System.out.println("Multi Comment");
+
+    }
+}
