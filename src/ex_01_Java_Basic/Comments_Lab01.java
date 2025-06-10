0a2d895195706a8483d40f8bfc08beb3e9755ae2
Arvind Kulkarni <aravinda.kulkarni@gmail.com>
2025-06-10 17:04:46 +0530

    addLab001

---
+++ src/ex_01_Java_Basic/Comments_Lab01.java
@@ -0,0 +1,17 @@
+package ex_01_Java_Basic;
+
+public class Comments_Lab01 {
+    /**
+     * Author : Arvind Kulkarni
+     * Course: Java
+     * This is a documentation Comment
+     * @param args command line arguments
+     * @return void
+     * Arvind
+     * Kulkarni
+     */
+    public static void main(String[] args) {
+        //Method Implementation
+        System.out.println("This is Arvind K");
+    }
+}
