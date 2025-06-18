f7633eb617957ad346ff9308617c6028a16f168f
Arvind Kulkarni <aravinda.kulkarni@gmail.com>
2025-06-18 06:20:33 +0530

    addLab001

---
+++ src/Literals/Lab_03.java
@@ -0,0 +1,9 @@
+package Literals;
+
+public class Lab_03 {
+    public static void main(String[] args) {
+        boolean is_married = true;
+        boolean is_married_amit = false;
+//         boolean is_married_amit = 123;
+    }
+}
---
+++ src/Literals/Lab_04.java
@@ -0,0 +1,8 @@
+package Literals;
+
+public class Lab_04 {
+    public static void main(String[] args) {
+                  int Enum = 9; // enum is keyword
+            System.out.println(Enum);
+    }
+}
