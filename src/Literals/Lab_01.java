fdab59cd90537007d1d8658b0a63a6b0bcd498bd
Arvind Kulkarni <aravinda.kulkarni@gmail.com>
2025-06-18 05:49:41 +0530

    addLab001

---
+++ src/Literals/Lab_01.java
@@ -0,0 +1,12 @@
+package Literals;
+
+public class Lab_01 {
+    public static void main(String[] args) {
+        int local=0;
+        System.out.println(local);
+        final float PI=3.14f;
+        //PI=3.13f;This is final in nature
+        System.out.println(PI);
+
+    }
+}
