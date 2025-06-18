b490950469929657c734c9a1d280a5b7142e980e
Arvind Kulkarni <aravinda.kulkarni@gmail.com>
2025-06-18 09:58:03 +0530

    addLab001

---
+++ src/Ternary_ifelse/ifelse07.java
@@ -0,0 +1,9 @@
+package Ternary_ifelse;
+
+public class ifelse07 {
+    public static void main(String[] args) {
+        int num=14;
+        String result=(num%2==0) ? "Even" : "Odd";
+        System.out.println(result);
+    }
+}
