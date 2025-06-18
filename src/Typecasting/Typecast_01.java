6af004047ee8ecc11422f8e5a5dd8d19691d0cdb
Arvind Kulkarni <aravinda.kulkarni@gmail.com>
2025-06-18 09:40:01 +0530

    addLab001

---
+++ src/Typecasting/Typecast_01.java
@@ -0,0 +1,11 @@
+package Typecasting;
+
+public class Typecast_01 {
+    public static void main(String[] args) {
+        byte b = 10;
+        int a = b; //  Widening -> Implicit Casting
+        int a1 = (int)b; // Widening -> Explicit Casting
+        //In Java, type casting means converting a variable from one data type to another.
+    }
+
+}
