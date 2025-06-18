4d1d2dbc9b7e7c1bf7e7257887385ff0be895915
Arvind Kulkarni <aravinda.kulkarni@gmail.com>
2025-06-18 09:44:13 +0530

    addLab001

---
+++ src/Ternary_ifelse/ifelse_01.java
@@ -0,0 +1,14 @@
+package Ternary_ifelse;
+
+public class ifelse_01 {
+    public static void main(String[] args) {
+        //The ternary operator in Java is a shortcut for if-else statements.
+        // It’s called ternary because it works with three parts.
+
+        // Age to vote?
+        int age = 18;
+        String canIVote = age >= 18 ? "Yes" : "No";
+        System.out.println(canIVote);
+    }
+
+}
