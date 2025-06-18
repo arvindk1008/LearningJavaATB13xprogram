1eb980adeb4496883514421e399259d36d812322
Arvind Kulkarni <aravinda.kulkarni@gmail.com>
2025-06-18 09:28:27 +0530

    addLab001

---
+++ src/Operators/Unary.java
@@ -0,0 +1,10 @@
+package Operators;
+
+public class Unary {
+    public static void main(String[] args) {
+        int a = 10;
+        boolean b = (10 == 11); // == -> Comparison 2 values
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
