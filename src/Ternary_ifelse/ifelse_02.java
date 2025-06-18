9d1f83a09fd50bed9ae16fbc74e3071c5bf2daf2
Arvind Kulkarni <aravinda.kulkarni@gmail.com>
2025-06-18 09:48:22 +0530

    addLab001

---
+++ src/Ternary_ifelse/ifelse_02.java
@@ -0,0 +1,10 @@
+package Ternary_ifelse;
+
+public class ifelse_02 {
+    public static void main(String[] args) {
+        int number=-5;
+        //Is number Positive or Negative
+        String result =number>0? "Positive":"Negative";
+        System.out.println(result);
+    }
+}
