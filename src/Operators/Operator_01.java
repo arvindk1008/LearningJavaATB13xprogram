3d943d428660f2aa1124f139c6d64eca0f9b864b
Arvind Kulkarni <aravinda.kulkarni@gmail.com>
2025-06-18 06:29:43 +0530

    addLab001

---
+++ src/Operators/Operator_01.java
@@ -0,0 +1,23 @@
+package Operators;
+
+public class Operator_01 {
+    public static void main(String[] args) {
+        // Arithmetic Operators
+        //        + (Addition)
+        //       - (Subtraction)
+        //       * (Multiplication)
+        //       / (Division)
+        //       % (Modulus) | Modulus - Remainder
+        float a = 20.00f;
+        int b = 3;
+        int c = 3;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a/c);
+
+        System.out.println("a+b");
+        System.out.println(a+b);
+    }
+}
