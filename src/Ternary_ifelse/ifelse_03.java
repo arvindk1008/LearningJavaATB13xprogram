e0dc81086818cd1229d513b79ee606f17b03ca42
Arvind Kulkarni <aravinda.kulkarni@gmail.com>
2025-06-18 09:52:12 +0530

    addLab001

---
+++ src/Ternary_ifelse/ifelse_03.java
@@ -0,0 +1,11 @@
+package Ternary_ifelse;
+
+public class ifelse_03 {
+    public static void main(String[] args) {
+        // The min number between two numbers by using ternary operator.
+        int x = 10;
+        int y = 20;
+                int min=x<y? x:y;
+        System.out.println(min);
+    }
+}
