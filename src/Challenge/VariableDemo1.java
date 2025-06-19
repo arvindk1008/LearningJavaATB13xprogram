72d9d3d06f532effafa27f7b7a50e4329bb8afab
Arvind Kulkarni <aravinda.kulkarni@gmail.com>
2025-06-19 07:47:34 +0530

    addLab001

---
+++ src/Challenge/VariableDemo1.java
@@ -0,0 +1,24 @@
+package Challenge;
+
+public class VariableDemo1 {
+    public static void main(String[] args) {
+
+        // Integer type
+        int age = 25;
+
+        // Double type (floating-point)
+        double heightInMeters = 1.75;
+
+        // Boolean type
+        boolean isStudent = true;
+
+        // Character type
+        char grade = 'A';
+
+        // Print all variable values
+        System.out.println("Age: " + age);
+        System.out.println("Height (in meters): " + heightInMeters);
+        System.out.println("Is Student: " + isStudent);
+        System.out.println("Grade: " + grade);
+    }
+}
