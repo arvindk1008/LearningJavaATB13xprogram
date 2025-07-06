23bd51633502b3011964f756414fae841efbf0d2
Arvind Kulkarni <aravinda.kulkarni@gmail.com>
2025-07-06 12:00:30 +0530

    addLab001

---
+++ src/basicprograms/Display_bio_student.java
@@ -0,0 +1,16 @@
+package basicprograms;
+
+public class Display_bio_student {
+    public static void main(String[] args) {
+        String name="Arvind Kulkarni";
+        String std="Eng";
+        String sub="Computer Applications";
+        String Sch="DPS";
+        String city="Pune";
+        System.out.println("Name:"+name);
+        System.out.println("Class:"+std);
+        System.out.println("Subject:"+sub);
+        System.out.println("School:"+Sch);
+        System.out.println("City:"+city);
+    }
+}
