db8a52d5b85ba2ab2dbecb0195034a5c84825ba3
Arvind Kulkarni <aravinda.kulkarni@gmail.com>
2025-07-06 11:34:08 +0530

    addLab001

---
+++ src/basicprograms/Display_Information.java
@@ -0,0 +1,12 @@
+package basicprograms;
+
+public class Display_Information {
+    public static void main(String[] args) {
+        //Display Information on below Screen
+        System.out.println("Name:");
+        System.out.println("Class:");
+        System.out.println("Roll No:");
+        System.out.println("Subject:");
+        System.out.println("School:");
+    }
+}
