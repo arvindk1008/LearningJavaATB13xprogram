3bf205f6d3252cf123d38758fb193bc37b6b0a24
Arvind Kulkarni <aravinda.kulkarni@gmail.com>
2025-07-06 11:34:08 +0530

    addLab001

---
+++ src/basicprograms/predict_output.java
@@ -0,0 +1,11 @@
+package basicprograms;
+
+public class predict_output {
+    public static void main(String[] args) {
+        System.out.println("India won ICC Cricket World Cup");
+        System.out.println("against");
+        System.out.println("West Indies");
+        System.out.print("in the year 1983 and 2011");
+
+    }
+}
