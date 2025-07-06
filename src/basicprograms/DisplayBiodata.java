b2015822064d97edfe12add431611a6c627ac739
Arvind Kulkarni <aravinda.kulkarni@gmail.com>
2025-07-06 11:29:08 +0530

    addLab001

---
+++ src/basicprograms/DisplayBiodata.java
@@ -0,0 +1,12 @@
+package basicprograms;
+
+public class DisplayBiodata {
+    public static void main(String[] args) {
+        System.out.println("Name:");
+        System.out.println("Father's Name:");
+        System.out.println("Date of Birth");
+        System.out.println("Blood Group");
+        System.out.println("Aadhar Card No:");
+        System.out.println("State");
+    }
+}
