e50d832f30dc3c468229ec4d3ffb2ef06e53d53f
Arvind Kulkarni <aravinda.kulkarni@gmail.com>
2025-07-06 11:49:47 +0530

    addLab001

---
+++ src/basicprograms/predictSum.java
@@ -0,0 +1,9 @@
+package basicprograms;
+
+public class predictSum {
+    public static void main(String[] args) {
+        int a=34,b=45;
+        System.out.println("The Sum of two numbers=");
+        System.out.println((a+b));
+    }
+}
